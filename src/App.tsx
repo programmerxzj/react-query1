088a10df7af1adf9d1ca0e818d874d3c57169489
programmerxzj <1638492106@qq.com>
2023-05-13 21:46:15 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import Login from './login';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
